1f204c1be77ac9c0e68ecb06d949621882783f7e
ByteBlender <byteblender21@gmail.com>
2025-03-26 14:33:41 +0100

    Init

---
+++ grammar.js
@@ -0,0 +1,17 @@
+/**
+ * @file Bg3 grammar for tree-sitter
+ * @author byteblender <byteblender21@gmail.com>
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "bg_3",
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: $ => "hello"
+  }
+});
